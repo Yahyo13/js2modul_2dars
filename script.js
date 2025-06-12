bf5bab4d2cb37c30ffee9392ec79e02f20b4a2b8
Yahyo <mirxodjaevyahyo@gmail.com>
2025-06-12 17:01:25 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,159 @@
+let button1 = document.querySelector(`.b1`)
+let button2 = document.querySelector(`.b2`)
+let button3 = document.querySelector(`.b3`)
+let button4 = document.querySelector(`.b4`)
+let button5 = document.querySelector(`.b5`)
+let button6 = document.querySelector(`.b6`)
+let button7 = document.querySelector(`.b7`)
+let button8 = document.querySelector(`.b8`)
+let button9 = document.querySelector(`.b9`)
+let button10 = document.querySelector(`.b10`)
+let button11 = document.querySelector(`.b11`)
+let button12 = document.querySelector(`.b12`)
+
+button1 = 45000
+button2 = 79000
+button3 = 34000
+button4 = 45000
+button5 = 62000
+button6 = 53000
+button7 = 36000
+button8 = 20000
+button9 = 43000
+button10 = 79000
+button11 = 149000
+button12 = 43000
+
+let bu1 = function(){
+    let a = prompt(`Qiyqiriq сет nechta olasiz?`)
+    let c = confirm(`${a} ta oldiz narxi ${button1*a} \n \n Maxsulotni zakaz qilasizmi?`)
+
+    switch(c){
+        case true:
+            alert(`Zakaz qabul qilindi`)
+        break
+        default:
+            alert(`idi`)    
+    }
+}
+let bu2 = function(){
+    let a = prompt(`Снек сет nechta olasiz?`)
+    let c = confirm(`${a} ta oldiz narxi ${button2*a} \n \n Maxsulotni zakaz qilasizmi?`)
+    switch(c){
+        case true:
+            alert(`Zakaz qabul qilindi`)
+        break
+        default:
+            alert(`idi`)    
+    }
+}
+let bu3 = function(){
+    let a = prompt(`Классик сет nechta olasiz?`)
+    let c = confirm(`${a} ta oldiz narxi ${button3*a} \n \n Maxsulotni zakaz qilasizmi?`)
+    switch(c){
+        case true:
+            alert(`Zakaz qabul qilindi`)
+        break
+        default:
+            alert(`idi`)    
+    }
+}
+let bu4 = function(){
+    let a = prompt(`Лонгер рингс сет nechta olasiz?`)
+    let c = confirm(`${a} ta oldiz narxi ${button4*a} \n \n Maxsulotni zakaz qilasizmi?`)
+    switch(c){
+        case true:
+            alert(`Zakaz qabul qilindi`)
+        break
+        default:
+            alert(`idi`)    
+    }
+}
+let bu5 = function(){
+    let a = prompt(`Биг сет nechta olasiz?`)
+   let c =  confirm(`${a} ta oldiz narxi ${button5*a} \n \n Maxsulotni zakaz qilasizmi?`)
+    switch(c){
+        case true:
+            alert(`Zakaz qabul qilindi`)
+        break
+        default:
+            alert(`idi`)    
+    }
+}
+let bu6 = function(){
+    let a = prompt(`Лестер сет nechta olasiz?`)
+    let c = confirm(`${a} ta oldiz narxi ${button6*a} \n \n Maxsulotni zakaz qilasizmi?`)
+    switch(c){
+        case true:
+            alert(`Zakaz qabul qilindi`)
+        break
+        default:
+            alert(`idi`)    
+    }
+}
+let bu7 = function(){
+    let a = prompt(`Скул сет nechta olasiz?`)
+    let c = confirm(`${a} ta oldiz narxi ${button7*a} \n \n Maxsulotni zakaz qilasizmi?`)
+    switch(c){
+        case true:
+            alert(`Zakaz qabul qilindi`)
+        break
+        default:
+            alert(`idi`)    
+    }
+}
+let bu8 = function(){
+    let a = prompt(`Комбо сет большой nechta olasiz?`)
+    let c = confirm(`${a} ta oldiz narxi ${button8*a} \n \n Maxsulotni zakaz qilasizmi?`)
+    switch(c){
+        case true:
+            alert(`Zakaz qabul qilindi`)
+        break
+        default:
+            alert(`idi`)    
+    }
+}
+let bu9 = function(){
+    let a = prompt(`Do’stlar 1х nechta olasiz?`)
+    let c = confirm(`${a} ta oldiz narxi ${button9*a} \n \n Maxsulotni zakaz qilasizmi?`)
+    switch(c){
+        case true:
+            alert(`Zakaz qabul qilindi`)
+        break
+        default:
+            alert(`idi`)    
+    }
+}
+let bu10 = function(){
+    let a = prompt(`Do’stlar 2х nechta olasiz?`)
+    let c = confirm(`${a} ta oldiz narxi ${button10*a} \n \n Maxsulotni zakaz qilasizmi?`)
+    switch(c){
+        case true:
+            alert(`Zakaz qabul qilindi`)
+        break
+        default:
+            alert(`idi`)    
+    }
+}
+let bu11 = function(){
+    let a = prompt(`Do’stlar 4х nechta olasiz?`)
+     let c = confirm(`${a} ta oldiz narxi ${button11*a} \n \n Maxsulotni zakaz qilasizmi?`)
+    switch(c){
+        case true:
+            alert(`Zakaz qabul qilindi`)
+        break
+        default:
+            alert(`idi`)    
+    }
+}
+let bu12 = function(){
+    let a = prompt(`Острый Do’stlar 1х nechta olasiz?`)
+    let c = confirm(`${a} ta oldiz narxi ${button12*a} \n \n Maxsulotni zakaz qilasizmi?`)
+    switch(c){
+        case true:
+            alert(`Zakaz qabul qilindi`)
+        break
+        default:
+            alert(`idi`)    
+    }
+}
